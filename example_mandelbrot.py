75678afe721481d35e0ab2d6a23057064460c371
Marius <59455568+Marius-450@users.noreply.github.com>
2020-03-09 13:48:39 -0400

    using display data is better.

--- example_mandelbrot.py
+++ example_mandelbrot.py
@@ -14,6 +14,7 @@ group = fractals.mandelbrot(display.width, display.height, 127, group=group)
 time.sleep(60)
 while True:
     rand_point = fractals.mandelbrot_points[randint(0, len(fractals.mandelbrot_points)-1)]
-    group = fractals.mandelbrot(240,240, 255, group, rand_point[0], rand_point[1])
+    group = fractals.mandelbrot(display.width, display.height, 255, group, rand_point[0], rand_point[1])
+    # 5 minutes pause.
     time.sleep(300)
     pass
